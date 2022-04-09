c36e39e63ac847a2ddd6cb8c6e7dd9499283b85c
ideepakrajput <deepakkumar18112001@gmail.com>
2022-04-09 20:43:57 +0530

    Program 8 Completed

--- Program8/app/src/main/java/com/deepak/program8/MainActivity.java
+++ Program8/app/src/main/java/com/deepak/program8/MainActivity.java
@@ -3,6 +3,8 @@ package com.deepak.program8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Cycle","OnCreate");
+        Toast.makeText(this, "OnCreate Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Cycle","OnStart");
+        Toast.makeText(this, "OnStart Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Cycle","OnResume");
+        Toast.makeText(this, "OnResume Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Cycle","OnPause");
+        Toast.makeText(this, "OnPause Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Cycle","OnStop");
+        Toast.makeText(this, "OnStop Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Cycle","OnDestroy");
+        Toast.makeText(this, "OnDestroy Called", Toast.LENGTH_SHORT).show();
     }
 }
