f6ba9a0a37b46c9da1223a2bb9a5741dceb52315
ideepakrajput <deepakkumar18112001@gmail.com>
2022-04-27 12:54:44 +0530

    commit

---
+++ Program9/app/src/main/java/com/deepak/program9/fore_color_text.java
@@ -0,0 +1,13 @@
+package com.deepak.program9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fore_color_text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
